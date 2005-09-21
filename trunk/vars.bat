de71d7c38737d93edb938c9c48ff9dece8180acc
poobah <poobah@1254b46e-66fb-0310-89d4-91d8628bef54>
2005-09-21 19:49:12 +0000

    Changes so MbUnit builds using MSBuild RC

--- trunk/vars.bat
+++ trunk/vars.bat
@@ -1,1 +1,3 @@
-set path=%path%;%cd%\bin\msbuild;%cd%\bin\WiX;%ProgramFiles%\TortoiseCVS
+@set path=%path%;%cd%\bin\WiX
+@set path=%path%;%ProgramFiles%\TortoiseCVS
+@set path=%path%;%SystemRoot%\Microsoft.NET\Framework\v2.0.50727
