42e13375ab07d5bf88d4287b1ae5193d167a7d3a
j9brown <j9brown@1254b46e-66fb-0310-89d4-91d8628bef54>
2006-08-04 20:16:09 +0000

    JIRA-33: RowTest should support rows with 1 value. This patch also supports trivial rows with 0 values because I can't think of any reason to deny it.

--- trunk/v2/mbunit/MbUnit.Framework/RowAttribute.cs
+++ trunk/v2/mbunit/MbUnit.Framework/RowAttribute.cs
@@ -3,77 +3,30 @@ using MbUnit.Core.Framework;
 
 namespace MbUnit.Framework
 {
+    /// <summary>
+    /// Provides a row of values using in conjunction with <see cref="RowTestAttribute" />
+    /// to bind values to the parameters of a row test method.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public sealed class RowAttribute : TestPatternAttribute
     {
         private Object[] row = null;
         private Type expectedException = null;
-        public RowAttribute(
-            Object a,
-            Object b)
+
+        /// <summary>
+        /// Provides a row of values using in conjunction with <see cref="RowTestAttribute" />
+        /// to bind values to the parameters of a row test method.
+        /// </summary>
+        /// <param name="row">The row of values to bind</param>
+        public RowAttribute(params object[] row)
         {
-            this.row = new Object[] { a, b };
-        }
-        public RowAttribute(
-            Object a,
-            Object b,
-            Object c)
-        {
-            this.row = new Object[] { a, b, c };
-        }
-        public RowAttribute(
-            Object a,
-            Object b,
-            Object c,
-            Object d)
-        {
-            this.row = new Object[] { a, b, c, d };
-        }
-        public RowAttribute(
-            Object a,
-            Object b,
-            Object c,
-            Object d,
-            Object e)
-        {
-            this.row = new Object[] { a, b, c, d, e };
-        }
-        public RowAttribute(
-            Object a,
-            Object b,
-            Object c,
-            Object d,
-            Object e,
-            Object f)
-        {
-            this.row = new Object[] { a, b, c, d, e, f };
+            this.row = row;
         }
 
-        public RowAttribute(
-             Object a,
-            Object b,
-            Object c,
-            Object d,
-            Object e,
-            Object f,
-            Object h)
-        {
-            this.row = new Object[] { a, b, c, d, e, f, h };
-        }
-
-        public RowAttribute(
-             Object a,
-            Object b,
-            Object c,
-            Object d,
-            Object e,
-            Object f,
-            Object h,
-            Object i)
-        {
-            this.row = new Object[] { a, b, c, d, e, f, h, i };
-        }
-
+        /// <summary>
+        /// Gets or sets the type of exception that is expected to be thrown when this
+        /// row is tested, or null if none.
+        /// </summary>
         public Type ExpectedException
         {
             get
@@ -86,6 +39,10 @@ namespace MbUnit.Framework
             }
         }
 
+        /// <summary>
+        /// Gets the row of values.
+        /// </summary>
+        /// <returns>The row of values</returns>
         public Object[] GetRow()
         {
             return this.row;
--- trunk/v2/mbunit/MbUnit.Framework/RowTestAttribute.cs
+++ trunk/v2/mbunit/MbUnit.Framework/RowTestAttribute.cs
@@ -3,6 +3,10 @@ using MbUnit.Core.Framework;
 
 namespace MbUnit.Framework
 {
+    /// <summary>
+    /// Declares a row test when applied to a test method along with one or more
+    /// <see cref="RowAttribute" /> attributes.
+    /// </summary>
 	[AttributeUsage(AttributeTargets.Method,AllowMultiple=false,Inherited=true)]
 	public sealed class RowTestAttribute : TestPatternAttribute
 	{
