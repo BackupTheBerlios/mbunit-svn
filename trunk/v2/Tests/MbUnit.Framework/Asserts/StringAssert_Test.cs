27e62a9ac1ca8f5e6d707854ba4c78518d6d3a1a
poobah <poobah@1254b46e-66fb-0310-89d4-91d8628bef54>
2006-08-09 14:06:34 +0000

    Inital commit for new unit test suite

---
+++ trunk/v2/Tests/MbUnit.Framework/Asserts/StringAssert_Test.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MbUnit.Framework;
+
+namespace MbUnit.Framework.Tests.Asserts
+{
+    [TestFixture]
+    public class StringAssert_Test
+    {
+        #region AreEqual
+        [Test]
+        public void AreEqualIgnoreCase()
+        {
+            StringAssert.AreEqualIgnoreCase("hello", "HELLO");
+        }
+
+        #endregion
+
+        #region Contains
+        [Test]
+        public void DoesNotContain()
+        {
+            StringAssert.DoesNotContain("hello", 'k');
+        }
+
+        #endregion
+
+        #region Emptiness
+        [Test]
+        public void IsEmpty()
+        {
+            StringAssert.IsEmpty("");
+        }
+
+        [Test]
+        public void IsNotEmpty()
+        {
+            StringAssert.IsNonEmpty(" ");
+        }
+
+        #endregion
+    }
+}
