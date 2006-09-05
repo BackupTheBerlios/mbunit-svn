c40caa6780528e63be047d90370b45daea8c58ff
ben2004uk <ben2004uk@1254b46e-66fb-0310-89d4-91d8628bef54>
2006-09-05 14:03:11 +0000

    MBUNIT-24: Added more assert tests.  ArrayAssert_Test, FileAssert_Test, WebAssert_Test Completed.

---
+++ trunk/v2/Tests/MbUnit.Framework/Asserts/DataAssert_Test.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace MbUnit.Framework.Tests.Asserts
+{
+    [TestFixture]
+    public class DataAssert_Test
+    {
+        [Test]
+        public void AreEqualDataColumn()
+        {
+            DataColumn d1 = new DataColumn("Col");
+            DataColumn d2 = d1;
+
+            
+            DataAssert.AreEqual(d1, d2);
+        }
+    }
+}
